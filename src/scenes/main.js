dff860665e1a7f6ae7ddc878fbd653deb418f186
Joe Hart <joseph.hart@lego.com>
2019-06-30 10:37:38 +0100

    Add animated bird and gravity

--- src/scenes/main.js
+++ src/scenes/main.js
@@ -1,25 +1,36 @@
 import Phaser from "phaser";
-import BirdImage from "../assets/bird.png";
-
-class MyScene extends Phaser.Scene {
-  init(data) {
-    this.scale.on("resize", this.resize);
+import BirdSpriteSheet from "../assets/birdsheet.png";
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super("MainScene");
   }
+
   preload() {
-    this.load.image("bird", BirdImage);
+    this.load.spritesheet("birdSprites", BirdSpriteSheet, {
+      frameWidth: 36,
+      frameHeight: 28
+    });
   }
   create(data) {
-    this.add.image(400, 300, "bird");
+    var config = {
+      key: "fly",
+      frames: this.anims.generateFrameNumbers("birdSprites", {
+        start: 0,
+        end: 10,
+        first: 1
+      }),
+      frameRate: 12,
+      repeat: -1
+    };
+    this.anims.create(config);
+
+    const { width, height } = this.game.renderer;
+    this.bird = this.physics.add.sprite(width / 2, height / 2, "birdSprites");
+    this.bird.setGravityY(4);
+    this.bird.anims.play("fly");
+    this.scale.on("resize", this.resize);
   }
   update(time, delta) {}
-  resize(
-    gameSize,
-    baseSize,
-    displaySize,
-    resolution,
-    previousWidth,
-    previousHeight
-  ) {}
 }
 
-export default MyScene;
+export default MainScene;
